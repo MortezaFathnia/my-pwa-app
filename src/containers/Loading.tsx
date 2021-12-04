c61346e1ec4244005b714d84ccfd4b072e857e2c
MortezaFathnia <fathniamorteza@gmail.com>
2021-12-04 21:10:10 +0330

    add service worker

--- src/containers/Loading.tsx
+++ src/containers/Loading.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import Loader from 'react-loader-spinner';
 
 const Loading = () => {
-  
   return (
     <div className="pt-3 text-center">
       <Loader type="ThreeDots" color="#00BFFF" height={100} width={100} />
